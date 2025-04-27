5af15151c828dfa23e6ace5ebdb5d4d1752d1bc6
arinahasandjanova <arinahasandjanova@gmail.com>
2025-04-28 00:31:11 +0400

    Update blockshape.cpp

--- source/blockshape.cpp
+++ source/blockshape.cpp
@@ -1,7 +1,14 @@
 #include "../headers/blockshape.h"
-
 BlockShape::BlockShape(const QVector<Cell> &c, const QString &clr, int i)
-    : cells(c), color(clr), id(i) {
+    : cells(c), color(clr), originalColor(clr), id(i) {
+}
+
+void BlockShape::changeColor(bool change) {
+    if (change) {
+        color = originalColor;
+    } else {
+        color = "#808080";
+    }
 }
 
 QDataStream &operator<<(QDataStream &out, const BlockShape::Cell &cell) {
